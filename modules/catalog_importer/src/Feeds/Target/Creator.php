4cb75266f6a26b41aefd30293aad5e18a2251320
amy-actually <amybwest@gmail.com>
2020-03-04 16:56:42 -0500

    add feeds target for author field

---
+++ modules/catalog_importer/src/Feeds/Target/Creator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\catalog_importer\Feeds\Target;
+
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\feeds\FieldTargetDefinition;
+use Drupal\feeds\Plugin\Type\Target\FieldTargetBase;
+
+/**
+ * Defines a creator field mapper.
+ *
+ * @FeedsTarget(
+ *   id = "creator",
+ *   field_types = {"creator"}
+ * )
+ */
+class Creator extends FieldTargetBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static function prepareTarget(FieldDefinitionInterface $field_definition) {
+    return FieldTargetDefinition::createFromFieldDefinition($field_definition)
+      ->addProperty('name')
+      ->addProperty('role');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function prepareValue($delta, array &$values) {
+    \Drupal::logger('catalog_importer')->notice("val $delta: <pre>@exclude</pre>", array(
+      '@exclude'  => print_r($values, TRUE),
+    )); 
+    $values['name'] = trim($values['name'], " \t\n\r\0\x0B-./\\");
+    $values['role'] = trim($values['role'], " \t\n\r\0\x0B-./\\");
+  }
+
+}
